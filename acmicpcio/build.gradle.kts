2026fb9cf619c0ae8e6cb7f09e3212b688dbaf88
young-hwang <ykcul02@gmail.com>
2023-05-02 22:14:18 +0900

    add: logback

--- acmicpcio/build.gradle.kts
+++ acmicpcio/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/ch.qos.logback/logback-core
+    implementation("ch.qos.logback:logback-core:1.4.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.24.2")
