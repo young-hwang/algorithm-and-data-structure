09598e62cbd3d89b4a198c76b3b9eaa4007dfae9
young-hwang <ykcul02@gmail.com>
2024-01-20 16:55:37 +0900

    add: MurMurHash 32bit

--- src/main/java/hash/MurmurHash3_32.java
+++ src/main/java/hash/MurmurHash3_32.java
@@ -1,23 +1,65 @@
 package hash;
 
 public final class MurmurHash3_32 {
-    private final int seed;
     private final int c1 = 0xcc9e2d51;
     private final int c2 = 0x1b873593;
     private final int e1 = 0x85ebca6b;
     private final int e2 = 0xc2b2ae35;
 
-    public MurmurHash3_32(int seed) {
-       this.seed = seed;
+    public int hashInt(int input) {
+        return this.hashInt(input, 0);
     }
 
-    public int hashInt(int input) {
+    public int hashInt(int input, int seed) {
         if (input == 0) {
             return 0;
         }
         int k = this.mixK(input);
-        int h = this.mixH(this.seed, k);
-        return this.avalanche(h, 4);
+        int h = this.mixH(seed, k);
+        return this.avalanche(h, Integer.SIZE / Byte.SIZE);
+    }
+
+    public int hashLong(long input) {
+        return this.hashLong(input, 0);
+    }
+
+    public int hashLong(long input, int seed) {
+        if (input == 0) {
+            return 0;
+        }
+        int low = (int) input;
+        int high = (int) (input >>> 32);
+
+        int k = this.mixK(low);
+        int h = this.mixH(seed, k);
+
+        k = this.mixK(high);
+        h = this.mixH(h, k);
+
+        return this.avalanche(h, Long.SIZE / Byte.SIZE);
+    }
+
+    public int hashString(CharSequence charSequence, int seed) {
+        if (charSequence == null || charSequence.length() == 0) {
+            return 0;
+        }
+
+        int h1 = seed;
+
+        for (int i = 1; i < charSequence.length(); i += 2) {
+            int k1 = charSequence.charAt(i - 1) | (charSequence.charAt(i) << 16);
+            k1 = this.mixK(k1);
+            h1 = this.mixH(h1, k1);
+        }
+
+        // deal with any remaining characters
+        if ((charSequence.length() & 1) == 1) {
+            int k1 = charSequence.charAt(charSequence.length() - 1);
+            k1 = this.mixK(k1);
+            h1 ^= k1;
+        }
+
+        return this.avalanche(h1, Character.SIZE / Byte.SIZE * charSequence.length());
     }
 
     public int mixK(int k) {
@@ -31,7 +73,7 @@ public final class MurmurHash3_32 {
         h ^= k;
         h = Integer.rotateLeft(h, 13);
         h = h * 5 + 0xe6546b64;
-	    return h;
+        return h;
     }
 
     public int avalanche(int h, int length) {
--- src/test/java/hash/MurmurHash3_32Test.java
+++ src/test/java/hash/MurmurHash3_32Test.java
@@ -8,12 +8,19 @@ class MurmurHash3_32Test {
 
     @Test
     void hashIntTest() {
-        MurmurHash3_32 murmurHash3_32 = new MurmurHash3_32(0);
+        MurmurHash3_32 murmurHash3_32 = new MurmurHash3_32();
         assertHashInt(murmurHash3_32.hashInt(0), 0);
         assertHashInt(murmurHash3_32.hashInt(1), -68075478);
         assertHashInt(murmurHash3_32.hashInt(2), 1085422463);
     }
 
+    @Test
+    void givenString_whenCalculateHash_thenEquals() {
+        MurmurHash3_32 murmurHash3_32 = new MurmurHash3_32();
+        assertHashInt(murmurHash3_32.hashString("test", 0), 1351191292);
+        assertHashInt(murmurHash3_32.hashString("test", 1), 2123230555);
+        assertHashInt(murmurHash3_32.hashString("hello", 0), -675079799);
+    }
     private static void assertHashInt(int hash, int expect) {
         assertEquals(expect, hash);
     }
