70a989d9a6069280fe584981e7ccab3fbfd0966e
young-hwang <ykcul02@gmail.com>
2024-04-21 12:51:53 +0900

    refactor: move folder

--- acmicpcio/src/main/java/_28_map/_10816/Main.java
+++ acmicpcio/src/main/java/_28_map/_10816/Main.java
@@ -1,10 +1,9 @@
-package _28_data_structure._10816;
+package _28_map._10816;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
